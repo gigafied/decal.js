f243ec70fa210e09a364bfa2c34b062d913def4f
DuLinRain <dulinrain@mail.hfut.edu.cn>
2021-02-24 12:02:42 +0800

    Update extend.js

--- tests/decal/utils/extend.js
+++ tests/decal/utils/extend.js
@@ -64,7 +64,7 @@ describe('extend', function () {
 
     expect(test2[3][0]).to.deep.equal(test[0])
   })
- 
+
   it('should properly defense Prototype Pollution', function () {
     let a = {}
     let b = JSON.parse('{"__proto__": {"pollute": "1"}}')
